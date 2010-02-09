87cb71c052f95b7da9e482b3c04e9d8fc104066a
Alan Medlar <ajm@gatsby.(none)>
2010-02-09 17:40:49 +0000

    simwalk works

--- messner2.py
+++ messner2.py
@@ -4,6 +4,7 @@ import sys, os, re, os.path, getopt, glob
 version = 0.3
 
 verbose         = False
+debug           = False
 force_flag      = False
 genehunter_flag = False
 simwalk_flag    = False
@@ -21,8 +22,9 @@ physical_map        = {}
 physical_positions  = {}
 
 # simwalk
-simwalk_rsid_decode = re.compile("^rs|s|\d\d+$")
-simwalk_rsid_illumina = re.compile("^(\-\d+)$")
+simwalk_rsid = re.compile("^rs|s|\d\d+$")
+#imwalk_rsid_illumina = re.compile("^rs|s|\d\d+$")
+#imwalk_rsid_decode = re.compile("^(\-\d+)$")
 simwalk_lod = re.compile("^\
 \s*\,\s*(\-?\d+\.\d+)\
 \s*\,\s*(\-?\d+\.\d+)\
@@ -43,13 +45,13 @@ class map_entry :
         self.probeid = probeid
 
 def usage() :
-    print >> sys.stderr, "usage: ./%s -m map_file [-sgh] [-l lod threshold]" % sys.argv[0]
+    print >> sys.stderr, "usage: %s -m map_file [-sgh] [-l lod threshold]" % sys.argv[0]
     print >> sys.stderr, "\t-m, --map\t\tmap file location (MANDATORY)"
-    print >> sys.stderr, "\t-x, --mapformat:\tformat of map file [decode (default), illumina]"
+#    print >> sys.stderr, "\t-x, --mapformat:\tformat of map file [decode (default), illumina]"
     print >> sys.stderr, "\t-l, --lod:\t\tset lod threshold, (default = 3)"
-    print >> sys.stderr, "\t-s, --simwalk:\t\tparse simwalk output files (cannot be used in combination with -g or -a)"
-    print >> sys.stderr, "\t-g, --genehunter:\tparse genehunter output files (cannot be used in combination with -s or -a)"
-    print >> sys.stderr, "\t-a, --allegro:\t\tparse allegro output files (cannot be used in combination with -g or -s)"
+    print >> sys.stderr, "\t-s, --simwalk:\t\tparse simwalk output files\t(cannot be used in combination with -g or -a)"
+    print >> sys.stderr, "\t-g, --genehunter:\tparse genehunter output files\t(cannot be used in combination with -s or -a)"
+    print >> sys.stderr, "\t-a, --allegro:\t\tparse allegro output files\t(cannot be used in combination with -g or -s)"
     print >> sys.stderr, "\t-f, --force:\t\tignore warnings and carry on regardless\
 \n\t\t\t\t(use of this option implies you are no longer doing science...)"
     print >> sys.stderr, "\t-h, --help:\t\tsee usage info\n"
@@ -58,6 +60,7 @@ def usage() :
 def read_simwalk(c, fname) :
     global format
     data = []
+    chr_str = "%02d" % c
 
     try :
         f = open(fname)
@@ -72,31 +75,45 @@ def read_simwalk(c, fname) :
         # lod score
         m = simwalk_lod.match(line)
         if m :
+            #print m.groups()
             tmp.append(m.groups())
             continue
-        
-        # marker name (a small nightmare, this depends on what alohomora uses...)
-        if format == 'decode' :
-            m = simwalk_rsid_decode.match(line)
-            if m :
-                marker = "SNP_A" + m.group(1)
-                marker = marker2rsid[marker]
 
-        elif format == 'illumina' :
-            m = simwalk_rsid_illumina.match(line)
-            if m :
-                if m.group(1).startswith('rs') :
-                    marker = m.group(1)
-                elif m.group(1).startswith('s') :
-                    marker = 'r' + m.group(1)
-                else :
-                    marker = 'rs' + m.group(1)
-        
+        m = simwalk_rsid.match(line)
         if m :
-            marker = m.group(1)
+            #print line,
+            stripped = line.strip()
+            if stripped.startswith('rs') :
+                marker = stripped
+            elif stripped.startswith('s') :
+                marker = 'r' + stripped
+            else :
+                marker = 'rs' + stripped
+        
+#        # marker name (a small nightmare, this depends on what alohomora uses...)
+#        if format == 'decode' :
+#            m = simwalk_rsid_decode.match(line)
+#            if m :
+#                marker = "SNP_A" + m.group(1)
+#                marker = marker2rsid[marker]
+#
+#        elif format == 'illumina' :
+#            m = simwalk_rsid_illumina.match(line)
+#            if m :
+#                if m.group(1).startswith('rs') :
+#                    marker = m.group(1)
+#                elif m.group(1).startswith('s') :
+#                    marker = 'r' + m.group(1)
+#                else :
+#                    marker = 'rs' + m.group(1)
+        
+            me = marker_map[marker]
             for t in tmp :
-                me = physical_map[(c, float(t[0]))]
                 data.append((marker, me.physical_position, float(t[1])))
+                #print marker,
+                #print me.physical_position,
+                #print t[1]
+            tmp = []
             
     return data
 
@@ -120,7 +137,8 @@ def read_all_allegro(chr) :
 
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hm:x:l:vsgaf", ["help","map=","mapformat=","lod=","verbose","simwalk","genehunter","allegro","force"])
+    #opts, args = getopt.getopt(sys.argv[1:], "hm:x:l:vsgaf", ["help","map=","mapformat=","lod=","verbose","simwalk","genehunter","allegro","force"])
+    opts, args = getopt.getopt(sys.argv[1:], "hm:l:vsgaf", ["help","map=","lod=","verbose","simwalk","genehunter","allegro","force"])
 except getopt.GetoptError, err:
     print >> sys.stderr, str(err)
     usage()
@@ -144,11 +162,11 @@ for o, a in opts:
         genehunter_flag = True
     elif o in ("-a","--allegro"):
         allegro_flag = True
-    elif o in ("-x","--mapformat"):
-        format = a.lower()
-        if format not in formats :
-            print >> sys.stderr, "Error: illegal map file format (see -h for details): %s" % format
-            sys.exit(-1)
+#    elif o in ("-x","--mapformat"):
+#        format = a.lower()
+#        if format not in formats :
+#            print >> sys.stderr, "Error: illegal map file format (see -h for details): %s" % format
+#            sys.exit(-1)
     elif o in ("-l","--lod"):
         try :
             PEAK_THRESHOLD = float(a)
@@ -160,7 +178,7 @@ for o, a in opts:
         assert False, "unhandled option"
 
 if (not (simwalk_flag ^ genehunter_flag ^ allegro_flag)) :
-    print >> sys.stderr, "%s: only one output file format can be specified" % sys.argv[0]
+    print >> sys.stderr, "%s: exactly one output file format must be specified" % sys.argv[0]
     sys.exit(-1)
 elif genehunter_flag :
     mode = "genehunter"
@@ -185,30 +203,30 @@ except IOError, err:
 # parse map file
 header = mf.readline()
 num_columns = len(header.split())
-if (((num_columns == 4 and format != "illumina")) \
-        or ((num_columns == 8 and format != "decode"))) \
-        and (force_flag != True):
-    print >> sys.stderr, "warning: are you sure %s is in %s format?" % (mapfile,format)
-    print >> sys.stderr, "\tif I'm wrong about this, then rerun with the -f or --force flag to run anyway\n"
-    sys.exit(-1)
+#if (((num_columns == 4 and format != "illumina")) \
+#        or ((num_columns == 8 and format != "decode"))) \
+#        and (force_flag != True):
+#    print >> sys.stderr, "warning: are you sure %s is in %s format?" % (mapfile,format)
+#    print >> sys.stderr, "\tif I'm wrong about this, then rerun with the -f or --force flag to run anyway\n"
+#    sys.exit(-1)
 
 for line in mf :
     data = line.split()
 
-    if format == "decode":
-        probe_id    = data[1]
-        phys_pos    = int(data[3])
-        rs_id       = data[4]
-        chromosome  = int(data[0])
-    elif format == "illumina":
-        probe_id    = data[0]
-        phys_pos    = int(data[3])
-        rs_id       = data[0]
-        chromosome  = int(data[2])
+    probe_id    = data[1]
+    phys_pos    = data[3]
+    rs_id       = data[4]
+    chromosome  = data[0]
+
+    if not rs_id.startswith('rs') :
+        continue
+
+    if len(rs_id) > 10 :
+        print >> sys.stderr, "Warning: %s is longer than 10 characters, so it is ambiguous in simwalk output files which truncates it to 8 characters" % rs_id
 
     me = map_entry(rs_id, chromosome, phys_pos, probe_id)
 
-    probe_map[probeid] = me
+    probe_map[probe_id] = me
     marker_map[rs_id] = me
     physical_map[(chromosome,phys_pos)] = me
 
@@ -245,6 +263,11 @@ for c in sorted(chromosomes) :
     lastnegtopos_phypos = None
 
     for marker,pos,lod in data :
+#        print marker,
+#        print pos,
+#        print lod
+#
+#        continue
 
         # is this a peak, if the same peak update the score
         if (lod > PEAK_THRESHOLD) and (lod > peak_value) :
@@ -273,7 +296,7 @@ for c in sorted(chromosomes) :
                 # if we are descending a peak, print the data
                 if peak :
                     print "chr%d %s %s %s_%s_%f" % \
-                        (c, lastnegtopos_phypos, pos, lastnegtopos, marker, peak_value)
+                        (c, lastnegtopos_phypos, pos, lastnegtopos_marker, marker, peak_value)
                     peak = False
                     peak_value = -1
             
