627fe39efeb8bc5bc60abfdeb82415ed36e9af79
Alan Medlar <ajm@gatsby.(none)>
2010-04-14 17:40:22 +0100

    v0.1, needs to be changed to custom track output + needs some notion of chromosome borders + clean up

--- ziggy.py
+++ ziggy.py
@@ -15,11 +15,12 @@ def usage() :
     -x, --genotypes:     filename of genotype file
     -p, --pedfile:       filename of pedigree file
     -h, --help:          show this help info
+    -v, --verbose:       be more verbose
 """ % sys.argv[0]
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hl:gsam:x:p:", ["help", "lod=", \
-            "genehunter", "simwalk", "allegro", "map=", "genotypes=", "pedfile="])
+    opts, args = getopt.getopt(sys.argv[1:], "hl:gsam:x:p:v", ["help", "lod=", \
+            "genehunter", "simwalk", "allegro", "map=", "genotypes=", "pedfile=", "verbose"])
 
 except getopt.GetoptError, err:
     print >> sys.stderr, str(err) # will print something like "option -a not recognized"
@@ -33,7 +34,6 @@ mapfilename = "../map.txt"
 pedfilename = "../pedfile.pro"
 genotypefilename = "../genotypes"
 debug = False
-debug = True
 
 for o, a in opts:
     if o in ("-h", "--help"):
@@ -53,6 +53,8 @@ for o, a in opts:
         pedfilename = a
     elif o in ("-x", "--genotypes") :
         genotypefilename = a
+    elif o in ("-v", "--verbose") :
+        debug = True
 
 
 # map
@@ -94,7 +96,7 @@ for line in f :
         continue
     data = line.split()
     fam,pid,pat,mat,sex,aff = data
-    if int(aff) == 1 :
+    if int(aff) == 2 :
         affection[int(pid)] = True
     else :
         affection[int(pid)] = False
@@ -110,8 +112,9 @@ except IOError, e :
     print >> sys.stderr, "you probably need to specify the genotypes file with the -x flag...\n"
     sys.exit(-1)
 
+omitted = set()
 header = f.readline()
-patients = header.strip().split()[1:]
+patients = map(int, header.strip().split()[1:])
 for line in f :
     # rsid, patient1, patient2...
     line = line.strip()
@@ -130,16 +133,22 @@ for line in f :
             continue
         current = ''.join(sorted(g[i]))
         if patients[i] not in affection :
+            omitted.add(patients[i])
             continue
         
         genotypes[m][ affection[patients[i]] ].add( current )
 
 f.close()
 
+#print >> sys.stderr, "\tomitted = %s" % str(omitted)
+
 print >> sys.stderr, "running messner..."
 program_name = "python /home/ajm/code/bioinformatics-tools/messner2.py"
 #program_name = "messner2"
 s,o = commands.getstatusoutput("%s -m %s -l %f %s" % (program_name, mapfilename, lod, program_arg))
+
+#print o
+#print "\n"
 
 print >> sys.stderr, "looking for homozygosity..."
 for line in o.split('\n') :
@@ -150,7 +159,7 @@ for line in o.split('\n') :
     chr,start,stop,guff = data
     m1,m2,lodscore = guff.split('_')
 
-    print line,
+    print line
     peak_start = marker_index[m1]
     peak_end   = marker_index[m2]
 
@@ -159,67 +168,99 @@ for line in o.split('\n') :
         if debug :
             print ""
 
+        if peak_end - peak_start <= 0 :
+            break
+
+#        print "peak_start = %d" % peak_start
+#        print "peak_end   = %d" % peak_end
+
         start_point = -1
         for i in range(peak_start, peak_end) :
             if markers[i] not in genotypes : # ie: this marker was removed by pedcheck or something...
                 if debug :
-                    print "\t%s not found" % (markers[i])
+                    print "\t[ignore] %s not found" % (markers[i])
                 continue
             affected_g = genotypes[markers[i]][True]
-            if (len(affected_g) == 1) and (affected_g[0] == 'AA' or affected_g[0] == 'BB') :
-                if affected_g[0] not in genotypes[markers[i]][False] :
+            if (len(affected_g) == 1) and (('AA' in affected_g) or ('BB' in affected_g)) :
+                if list(affected_g)[0] not in genotypes[markers[i]][False] :
                     if debug :
-                        print "\t%s homozygous in affected only" % markers[i]
+                        print "\t[bingo ] %s homozygous in affected only (%s , %s)" \
+                                % (markers[i], str(tuple(affected_g)), str(tuple(genotypes[markers[i]][False])))
                     peak_start = start_point = i
-                    hmode = affected_g[0]
+                    hmode = list(affected_g)[0]
                     break
                 else :
                     if debug :
-                        print "\t%s homozygous, but not just in affected" % markers[i]
+                        print "\t[ skip ] %s homozygous, but not just in affected (%s , %s)" \
+                                % (markers[i], str(tuple(affected_g)), str(tuple(genotypes[markers[i]][False])))
             else :
                 if debug :
-                    print "\t%s not homozygous in affected" % markers[i]
+                    print "\t[ skip ] %s not homozygous in affected (%s , %s)" \
+                            % (markers[i], str(tuple(affected_g)), str(tuple(genotypes[markers[i]][False])))
 
         if start_point == -1 :
 #            print line,
-            print ": no homozygosity"
+            #print ": no homozygosity"
             break
 
         # go as far left as possible
-        next = start_point - 1
+        next = start_point
         while True :
+            next -= 1
             if next < 0 :
                 break
-            g_aff = genotypes[next][True]
-            g_non = genotypes[next][False]
+            if markers[next] not in genotypes :
+                if debug :
+                    print "\t[ignore] %s not found" % (markers[next])
+                continue
+            g_aff = list(genotypes[markers[next]][True])
+            g_non = list(genotypes[markers[next]][False])
             if (len(g_aff) == 1) and (g_aff[0] == hmode) and (hmode not in g_non) :
-                next -= 1
+                if debug :
+                    print "\t[homoz ] %s ( <-- )" % markers[next]
+                #next -= 1
             else :
+                if debug :
+                    print "\t[ end  ] ( <-- )"
                 break
 
         homoz_start = markers[next + 1]
 
-        next = start_point + 1
+        next = start_point
         # go as far right...
         while True :
+            next += 1
             if next > len(markers) :
                 break
-            g_aff = genotypes[next][True]
-            g_non = genotypes[next][False]
+            if markers[next] not in genotypes :
+                if debug :
+                    print "\t[ignore] %s not found" % (markers[next])
+                continue
+            g_aff = list(genotypes[markers[next]][True])
+            g_non = list(genotypes[markers[next]][False])
             if (len(g_aff) == 1) and (g_aff[0] == hmode) and (hmode not in g_non) :
-                next += 1
+                if debug :
+                    print "\t[homoz ] %s ( --> )" % markers[next]
+                #next += 1
             else :
+                if debug :
+                    print "\t[ end  ] ( --> )"
                 break
 
         homoz_end = markers[next - 1]
 
 #        print line,
+        print "(",
         print homoz_start,
-        print "--",
-        print homoz_end
+        print "-->",
+        print homoz_end,
+        print ")"
+
+        peak_start = next
 
     if debug :
         print ""
 
-    sys.exit()
-
+#    sys.exit()
+if debug :
+    print "done"
